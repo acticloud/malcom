02c17bb452f9dda50c7f7ada8c0619d2436db3d1
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2018-07-19 14:57:40 +0200

    Just no. Experiments should be defined by configuration

--- malcom/malcom.py
+++ malcom/malcom.py
@@ -39,39 +39,39 @@ def init_parser():
     return parser
 
 
-def plot_tpch10_select_error():
-    pth = "./docs/figs/tpch10/"
-    d = 'tpch10'
-    for i in set(range(1, 23)) - set([10, 13]):
-        q = "0{}".format(i) if i < 10 else str(i)
-        o = "./docs/figs/tpch10/tpch10_sel{}_error.pdf".format(q)
-        experiments.plot_select_error_air(d, q, ntrain=200, path=pth, output=o)
-
-
-def plot_tpch10_mem_error():
-    pth = "./docs/figs/tpch10/"
-    d = 'tpch10'
-    for i in set(range(1, 23)) - set([10, 13]):
-        q = "0{}".format(i) if i < 10 else str(i)
-        o = "./docs/figs/tpch10/tpch10_q{}_memerror.pdf".format(q)
-        experiments.plot_mem_error_air(d, q, ntrain=200, path=pth, output=o)
-
-
-def plot_airtraffic_select_error():
-    f = "./docs/figs/airtraffic/airtraffic_sel{}_error.pdf"
-    for q in ['04', '09', '10', '11', '15.1', '19.1']:
-        o = f.format(q.replace('.', '_'))
-        experiments.plot_select_error_air('airtraffic', q, trainq=q, output=o)
-
-
-def plot_airtraffic_mem_error():
-    f = "./docs/figs/airtraffic/airtraffic_q{}_memerror.pdf"
-    for q in ['04', '09', '10', '11', '15.1', '19.1']:
-        testq = q
-        out = f.format(q.replace('.', '_'))
-        experiments.plot_mem_error_air('airtraffic', testq, trainq=q, output=out)
-
-
+# def plot_tpch10_select_error():
+#     pth = "./docs/figs/tpch10/"
+#     d = 'tpch10'
+#     for i in set(range(1, 23)) - set([10, 13]):
+#         q = "0{}".format(i) if i < 10 else str(i)
+#         o = "./docs/figs/tpch10/tpch10_sel{}_error.pdf".format(q)
+#         experiments.plot_select_error_air(d, q, ntrain=200, path=pth, output=o)
+#
+#
+# def plot_tpch10_mem_error():
+#     pth = "./docs/figs/tpch10/"
+#     d = 'tpch10'
+#     for i in set(range(1, 23)) - set([10, 13]):
+#         q = "0{}".format(i) if i < 10 else str(i)
+#         o = "./docs/figs/tpch10/tpch10_q{}_memerror.pdf".format(q)
+#         experiments.plot_mem_error_air(d, q, ntrain=200, path=pth, output=o)
+#
+#
+# def plot_airtraffic_select_error():
+#     f = "./docs/figs/airtraffic/airtraffic_sel{}_error.pdf"
+#     for q in ['04', '09', '10', '11', '15.1', '19.1']:
+#         o = f.format(q.replace('.', '_'))
+#         experiments.plot_select_error_air('airtraffic', q, trainq=q, output=o)
+#
+#
+# def plot_airtraffic_mem_error():
+#     f = "./docs/figs/airtraffic/airtraffic_q{}_memerror.pdf"
+#     for q in ['04', '09', '10', '11', '15.1', '19.1']:
+#         testq = q
+#         out = f.format(q.replace('.', '_'))
+#         experiments.plot_mem_error_air('airtraffic', testq, trainq=q, output=out)
+#
+#
 if __name__ == '__main__':
     parser = init_parser()
     args = parser.parse_args()
@@ -90,4 +90,4 @@ if __name__ == '__main__':
     # plot_airtraffic_select_error()
     # plot_airtraffic_mem_error()
     # plot_tpch10_mem_error()
-    experiments.plot_allmem_tpch10()
+    # experiments.plot_allmem_tpch10()
