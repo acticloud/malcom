73f97e8b84d6b314de77a1ab5651b7f29384426d
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2018-07-27 12:34:16 +0200

    Rename tests directory
    Also ignore setuptools generated directories

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,8 +6,10 @@ from malcom import utils
 
 class TestUtils():
     def test_json_reading(self):
-        fn = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                          'data/test_json_reading')
+        fn = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'data/test_json_reading'
+        )
         with open(fn) as input_file:
             ln = utils.Utils.readJsonObject(input_file)
             assert ln is not None
