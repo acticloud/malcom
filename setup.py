20331fbcc5c53dff603650b23f06f06fcf191c8a
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2018-07-27 13:36:56 +0200

    Proper version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="malcom",
-    version="0.2-dev",
+    version="0.2.dev0",
     packages=find_packages(),
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
