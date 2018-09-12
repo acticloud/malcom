277be51231de475f1e35cce058f1ad0b0c9ef04e
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2018-09-12 12:12:31 +0200

    Rename tpch_sql to more appropriate dummy_sql

---
+++ dummy_sql/Q20.sql
@@ -0,0 +1,1 @@
+SELECT 'Query Q20', count(*) as recs from generate_series(10, 1000) a, generate_series(10, 1000) b, generate_series(10, 5000) c where a.value % b.value = 0 and (a.value + b.value ) % c.value = 0;
--- tpch_sql/Q05.sql
+++
@@ -1,1 +0,0 @@
-SELECT 'Query Q05';
